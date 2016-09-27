36ae660cdfa1b15b223d9cb14befe188df58f088
bujmar-3 <marz@hotmail.se>
2016-09-27 22:17:02 +0200

    Data logik
    bygger upp logik om vi kan hämta från "cashe" eller om vi måste koppla
    upp oss mot API, läs kommentarer för mer info.

--- src/lab2/data/City.java
+++ src/lab2/data/City.java
@@ -4,17 +4,19 @@ public class City {
 	private double lat = 0;
 	private double lon = 0;
 	private double msl = 0;
-	private double time = 0;
+	private double hour = 0; /*Time for the weather forecast*/
 	private double temp = 0;
+	private double reqTime = 0; /*Time this was requested against API*/
 	/**
 	 * Constructor for the City objects, used to cashe the data for diffrent citys.
 	 * **/
-	public City(double time, double lat, double lon, double msl, double temp){
-		this.time = time;
+	public City(double hour, double reqTime, double lat, double lon, double msl, double temp){
+		this.hour = hour;
 		this.lat = lat;
 		this.lon = lon;
 		this.msl = msl;
 		this.temp = temp;
+		this.reqTime = reqTime;
 	}
 	public double getLat(){
 		return this.lat;
@@ -26,10 +28,13 @@ public class City {
 		return this.msl;
 	}
 	public double getTime(){
-		return this.time;
+		return this.hour;
 	}
 	public double getTemp(){
 		return this.temp;
+	}
+	public double getReqTime(){
+		return this.reqTime;
 	}
 
 }
--- src/lab2/data/Data.java
+++ src/lab2/data/Data.java
@@ -1,30 +1,47 @@
 package lab2.data;
 import java.util.Observable;
+import java.util.Vector;
 import java.io.*;
 import java.net.*;
+import java.util.Date;
 
 
 public class Data extends Observable {
 	private Vector<City> Cities;
-	public Data(){
-		
-		int timeLimit = ;
-		if City.time <= timeLimit{
-			return City.getTemp();
-		}else
-		{
-			
-		}
-		
+	private double agingTime;
+
+	
+	public Data(double agingTime){
+		Cities = new Vector<City>();
 	}
 	/**
 	 * getData, gets the information needed from the yr.no API
-	 * @param lat = latitude, lon = longitude, msl = meters over sea level.
+	 * @param lat = latitude, lon = longitude, msl = meters over sea level, time = time for forcast.
+	 * @return temprature
 	 * **/
-	public void getData(double lat, double lon, double msl) throws Exception{
-		URL yr = new URL("http://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10;lon=9.58;msl=70");
-		URLConnection yrConnection = yr.openConnection();
+	public double getData(double lat, double lon, double msl, double time) throws Exception{
+		for (City town : Cities){
+			double cityLat = town.getLat();
+			double cityLon = town.getLon();
+			double cityMsl = town.getMsl();
+			double cityTime = town.getTime();
+			if(cityLat == lat & cityLon == lon & cityMsl == msl & cityTime == time & (this.getTime()-town.getReqTime())<this.agingTime){
+				return town.getTemp();
+			}
+			else if(cityLat == lat & cityLon == lon & cityMsl == msl & cityTime == time & (this.getTime()-town.getReqTime())>this.agingTime){
+				/*Staden finns men data är för gammalt, släng stad från vector och skapa ny*/
+			}
+			else{
+				/*Staden finns inte ännu skapa stad*/
+			}
+		}
 		
+	}
+	private double getTime(){
+		/*Should return current time (current time IRL)*/
+	}
+	public void setAgingTime(double agingTime){
+		this.agingTime = agingTime;
 	}
 
 }
