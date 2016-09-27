7a5c9bdfb1d50ff00c9c35245cef13b494e6c910
bujmar-3 <marz@hotmail.se>
2016-09-27 14:09:24 +0200

    Main
    Gjorde så att main skapar obejekt av alla klasserna

--- src/lab2/main/Main.java
+++ src/lab2/main/Main.java
@@ -1,5 +1,14 @@
 package lab2.main;
 
+import lab2.data.Data;
+import lab2.controller.Controller;
+import lab2.view.Gui;
+
 public class Main {
-asdas
+
+	public static void main(){
+		Data model = new Data();
+		Controller controller = new Controller();
+		Gui gui = new Gui();
+}
 }
