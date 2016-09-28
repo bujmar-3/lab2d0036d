4e33a7b75d6585604a43b028c69c03f2bc7ece46
bujmar-3 <marz@hotmail.se>
2016-09-28 18:04:23 +0200

    API data hämtning
    Försöker koda en metod som hämtar data från API, hjälp behövs.

--- src/lab2/data/Data.java
+++ src/lab2/data/Data.java
@@ -1,6 +1,10 @@
 package lab2.data;
 import java.util.Observable;
 import java.util.Vector;
+import org.w3c.dom.*;
+
+import javax.xml.parsers.*;
+
 import java.io.*;
 import java.net.*;
 import java.util.Date;
@@ -29,6 +33,8 @@ public class Data extends Observable {
 				return town.getTemp();
 			}
 			else if(cityLat == lat & cityLon == lon & cityMsl == msl & cityTime == time & (this.getTime()-town.getReqTime())>this.agingTime){
+				Cities.remove(town);
+				
 				/*Staden finns men data är för gammalt, släng stad från vector och skapa ny*/
 			}
 			else{
@@ -37,11 +43,37 @@ public class Data extends Observable {
 		}
 		
 	}
+	
 	private double getTime(){
 		/*Should return current time (current time IRL)*/
 	}
+	
 	public void setAgingTime(double agingTime){
 		this.agingTime = agingTime;
+	}
+	/**xmlParse ska hämta ut data från API skapa ett city obeject av datan och retunera detta objekt
+	 * 
+	 * **/
+	private City xmlParse(String url, double time){
+		DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
+		Document xmlDoc = dbuilder.parse(new URL(url).openStream());
+		NodeList timeList = xmlDoc.getElementsByTagName("time");
+			for(int i=0;i<timeList.getLength();i++){
+				Element t = (Element) timeList.item(i);
+				String from = t.getAttribute("from");
+				String to = t.getAttribute("to");
+				double timeFrom = strToDouble(from);
+				double timeTo = strToDouble(to);
+				/*Kolla om det är tiden vi söker*/
+				if(time>timeFrom & time<timeTo){
+					t.getAttributeNode("")
+				}
+			}
+		
+	}
+	private double strToDouble(String str){
+		
 	}
 
 }
