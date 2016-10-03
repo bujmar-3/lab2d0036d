26e2519b91d366f6a30e56e4a4cacf98c1f99380
bigmtfb <Blomman@Mattiass-MacBook-Pro-2.local>
2016-10-03 17:13:19 +0200

    gui

--- src/lab2/view/Gui.java
+++ src/lab2/view/Gui.java
@@ -1,13 +1,56 @@
 package lab2.view;
-import javax.swing.JFrame;
 
+import lab2.controller.Controller;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Date;
+import java.util.Vector;
+import javax.swing.*;
+
+// The Gui
 public class Gui {
-	public static void main(String[] args){
-		Ginp gobj = new Ginp();
-		gobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gobj.setSize(640, 320);
-		gobj.setVisible(true);
-		
+	private JFrame weatherFrame;
+	private JLabel head;
+	private JLabel status;
+	private JPanel ctrl;
+	
+	//Constructor
+	public Gui(){
+		buildGui();
 	}
-
+	
+	
+	// Main method
+	// Creates window
+	public static void main(String[] args){
+		Date date = new Date();
+		System.out.println("Date: "+ date);
+		Gui YrCoordz = new Gui();
+		lab2.data.Data.getData();
+	}
+	
+	public void buildGui(){
+		weatherFrame = new JFrame("climate climax");
+		weatherFrame.setLayout(new GridLayout(3, 1));
+		weatherFrame.setSize(333,333);
+	    weatherFrame.addWindowListener(new WindowAdapter() {
+		    public void windowClosing(WindowEvent windowEvent){
+		    	System.exit(0);
+		    }
+	    });
+			
+		head = new JLabel("",JLabel.TOP);
+	    
+		ctrl = new JPanel();
+	    ctrl.setLayout(new FlowLayout());
+		
+	    status = new JLabel("", JLabel.TOP);
+	    status.setSize(350,100);
+	    
+	    weatherFrame.add(head);
+	    weatherFrame.add(status);
+	    weatherFrame.add(ctrl);
+	    weatherFrame.setVisible(true);  
+	}
+	
 }
