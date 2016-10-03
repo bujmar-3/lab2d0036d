05c12e0f27fd0c23fb304d33d329d9d617679479
bujmar-3 <marz@hotmail.se>
2016-10-03 16:25:14 +0200

    Simpler data
    fixar data klassen

--- src/lab2/data/Data.java
+++ src/lab2/data/Data.java
@@ -1,6 +1,4 @@
 package lab2.data;
-import java.util.Observable;
-import java.util.Vector;
 import org.w3c.dom.*;
 
 import javax.xml.parsers.*;
@@ -12,70 +10,57 @@ import java.util.Date;
 import javax.xml.bind.DatatypeConverter;
 
 
-public class Data extends Observable {
-	private Vector<City> Cities;
-	private double agingTime;
+public class Data {
+	/** getTemp 
+	 *  Hämtar tempratur från API med angiven url och tid.
+	 * **/
 
-	
-	public Data(double agingTime){
-		Cities = new Vector<City>();
-	}
-	/**
-	 * getData, gets the information needed from the yr.no API
-	 * @param lat = latitude, lon = longitude, msl = meters over sea level, time = time for forcast.
-	 * @return temprature
-	 * **/
-	public double getData(double lat, double lon, double msl, double time) throws Exception{
-		for (City town : Cities){
-			double cityLat = town.getLat();
-			double cityLon = town.getLon();
-			double cityMsl = town.getMsl();
-			double cityTime = town.getTime();
-			if(cityLat == lat & cityLon == lon & cityMsl == msl & cityTime == time & (this.getTime()-town.getReqTime())<this.agingTime){
-				return town.getTemp();
-			}
-			else if(cityLat == lat & cityLon == lon & cityMsl == msl & cityTime == time & (this.getTime()-town.getReqTime())>this.agingTime){
-				Cities.remove(town);
-				
-				/*Staden finns men data är för gammalt, släng stad från vector och skapa ny*/
-			}
-			else{
-				/*Staden finns inte ännu skapa stad*/
-			}
-		}
-		
-	}
-	
-	private double getTime(){
-		/*Should return current time (current time IRL)*/
-	}
-	
-	public void setAgingTime(double agingTime){
-		this.agingTime = agingTime;
-	}
-	/**xmlParse ska hämta ut data från API skapa ett city obeject av datan och retunera detta objekt
-	 * 
-	 * **/
-	private City xmlParse(String url, double time){
+	public String getTemp(String url, String time){
+		String temprature = "none";
+		try{
 		DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
-		Document xmlDoc = dbuilder.parse(new URL(url).openStream());
+		Document xmlDoc = dbuilder.parse(url);
 		NodeList timeList = xmlDoc.getElementsByTagName("time");
-			for(int i=0;i<timeList.getLength();i++){
-				Element t = (Element) timeList.item(i);
-				String from = t.getAttribute("from");
-				String to = t.getAttribute("to");
-				/*Här ska vi ta ut tider och se till att rätt data hämtas. Vet dock inte hur vi översätter från XML dateTime till något användbart*/
-				Calendar checkTime = parseDateTime(to);
-				/*Kolla om det är tiden vi söker*/
-				if(time>timeFrom & time<timeTo){
-					t.getAttributeNode("")
+		NodeList tempList = xmlDoc.getElementsByTagName("temprature");
+		for(int i = 0; i < timeList.getLength(); i++){
+			Node tempNode = tempList.item(i);
+			Node timeNode = timeList.item(i);
+			if(tempNode.getNodeType() == Node.ELEMENT_NODE){
+				Element tempElement = (Element) tempNode;
+				Element timeElement = (Element) timeNode;
+				String timeFromAttribute = timeElement.getAttribute("from");
+				String timeToAttribute = timeElement.getAttribute("to");
+				if(time.equals(timeFromAttribute) && time.equals(timeToAttribute)){
+					temprature = tempElement.getAttribute("value");
+					return temprature;
 				}
 			}
-		
+		}
+	}catch(Exception e){
+		System.out.println("Error in getTemp");
 	}
-	private int parseString(String str){
-		
+		return temprature;
 	}
+	/**
+	 * createUrl(lat, lon, msl)
+	 * Creates an URL used to get wanted data from API
+	 * @return URL String to get data from
+	 * */
+	public String createUrl(String lat, String lon, String msl){
+		String url = "http://api.met.no/weatherapi/locationforecast/1.9/?l"+"lat="+lat+";lon="+lon+";msl="+msl;
+		return url;
+	}
+	
+	pulic Array getLoc(String place){
+		String[] locArray;
+		try{
+			DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+		}catch(Exception e){
+			
+		}
+	}
+	
+		
 
 }
